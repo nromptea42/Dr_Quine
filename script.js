99af7f733976f0dbd264a371ae7f4e4c9118d7be
nromptea42 <natrptx@hotmail.fr>
2018-01-21 20:03:10 +0100

    sully.c

--- script.js
+++
@@ -1,5 +0,0 @@
-var myArgs = process.argv.slice(2);
-
-for(var i = 0; i < myArgs[0].length; i++) {
-	process.stdout.write(myArgs[0].charCodeAt(i) + ", ");
-}
--- test.c
+++ test.c
@@ -1,8 +1,32 @@
+
+#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <fcntl.h>
-// comment
-int main(void) {
-	int fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	dprintf(fd, "BONJOUR JE SUIS LE PAIN");
+#define FLAGS O_WRONLY | O_CREAT | O_TRUNC
+#define NAME "Sully_%i.c"
+#define COMPILE "clang -Wall -Wextra -Werror Sully_%i.c -o Sully_%i"
+#define SOURCE "#include <unistd.h>%c#include <stdio.h>%c#include <stdlib.h>%c#include <fcntl.h>%c#define FLAGS O_WRONLY | O_CREAT | O_TRUNC%c#define NAME %cSully_%ci.c%c%c#define COMPILE %cclang -Wall -Wextra -Werror Sully_%ci.c -o Sully_%ci%c%c#define SOURCE %c%s%c%cint main() { int fd;%cint i = %i;%cchar *f, *str, *name; if (i <= 0) { exit(1); } asprintf(&f, NAME, i); while (access(f, F_OK)!=-1 && i--) { if (i < 0) { exit(1); } asprintf(&f, NAME, i); } if ((fd = open(f, FLAGS, 0600)) == -1) { exit(1); } char *s = SOURCE;%cdprintf(fd, s, 10, 10, 10, 10, 10, 34, 37, 34, 10, 34, 37, 37, 34, 10, 34, s, 34, 10, 10, (i == -1) ? 0 : i, 10, 10, 34, 37, 34); asprintf(&str, COMPILE, i, i); asprintf(&name, %c./Sully_%ci%c, i); system(str); system(name); }"
+int main() {
+	int fd;
+	int i = 5;
+	char *f, *str, *name;
+	if (i <= 0) {
+		exit(1);
+	}
+	while (access(f, F_OK)!=-1 && i--) {
+		if (i < 0) {
+			exit(1);
+		}
+		asprintf(&f, NAME, i);
+	}
+	if ((fd = open(f, FLAGS, 0600)) == -1) {
+		exit(1);
+	}
+	char *s = SOURCE;
+	dprintf(fd, s, 10, 10, 10, 10, 10, 34, 37, 34, 10, 34, 37, 37, 34, 10, 34, s, 34, 10, 10, (i == -1) ? 0 : i, 10, 10, 34, 37, 34);
+	asprintf(&str, COMPILE, i, i);
+	asprintf(&name, "./Sully_%i", i);
+	system(str);
+	system(name);
 }
